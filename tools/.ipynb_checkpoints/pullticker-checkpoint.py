ff817c7a9327a02f8746ad246b98fe8297564468
Matthew Bromley <matthew.bromley@gmail.com>
2021-07-27 15:58:25 +0100

    New function to calculate ticker name and notebook updated
    New function to calculate the name from its ticker symbol
    
    Playground notebook example compares multiple different commodities by their score

--- tools/.ipynb_checkpoints/pullticker-checkpoint.py
+++ tools/.ipynb_checkpoints/pullticker-checkpoint.py
@@ -1,12 +1,20 @@
 import yfinance as yf
 import pandas as pd
 import datetime
+import json
+from ast import literal_eval
 
 def pull_symbol(name):
     print("Pulling Information on the Symbol " + name + "/n")
     stock = yf.Ticker(name)
     history = stock.history(period="1y", interval="1h")
     return history;
+
+def pull_symbol_name(name):
+    stock = yf.Ticker(name)
+    stockinfo = stock.info
+    #stockparse = literal_eval(stockinfo)
+    return stockinfo["shortName"]
 
 def pull_symbol_history():
     return;
