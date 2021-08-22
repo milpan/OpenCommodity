9b6049ace7c6c84e54be26f5f9e9c422f052a9f9
Matthew Bromley <matthew.bromley@gmail.com>
2021-08-22 18:26:23 +0100

    Sharpe Ratio is now calculated
    Sharpe ratio is now able to be returned from Historic Price Data, example is located in PortfolioPlaybook.ipynb

--- metrics/sharpe.py
+++ metrics/sharpe.py
@@ -1,13 +1,28 @@
 import pandas as pd
 
 def calculate_sharpe(PortfolioHistory, ListofSymbol):
+    #method is clunky, will refine this soon...
     iterator = 0;
-    #Lets first reorganise our List of Portfolio Data
+    newHistory = []
+    PctChange = []
+    meanPctChange = []
+    stdPctChange = []
     for history in PortfolioHistory:
-        history['Symbol'] = pd.Series(ListofSymbol[iterator], index=history.index);
+        temphistory = pd.DataFrame(data=history['Close'])
         iterator=iterator+1;
-    #Concatanate the Result and Pull out the Close and Symbol Info
-    result = pd.concat(PortfolioHistory, axis=0)
-    result = result[['Close', 'Symbol']]
-    return result
+        newHistory.append(temphistory)
+    #concatanate the result into one large dataframe
+    result = pd.concat(newHistory, axis=1)
+    iterator = 1;
+    #set the appropriate labels
+    result.columns = ListofSymbol
+    #calculate the change in each symbol
+    for history in newHistory:
+        history['PctChange'] = history['Close'].pct_change() * 100
+        #calculate the mean of this percentage change
+        history['Mean'] = history['PctChange'].rolling(365, min_periods=2).mean()
+        history['Std'] = history['PctChange'].rolling(365, min_periods=2).std()
+        history['Sharpe'] = history['PctChange']/history['Std']
+
+    return newHistory;
             
